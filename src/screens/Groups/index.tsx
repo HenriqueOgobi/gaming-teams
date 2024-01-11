b93f94cbd164c863562cbd14c5bb47a2f6546bc6
Henrique Oliveira Gobi <102251338+HenriqueOgobi@users.noreply.github.com>
2024-01-11 12:00:23 -0300

    Initial commit

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export  function Groups() {
+  return (
+      <Text>Groups! Hello React-Native</Text>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
