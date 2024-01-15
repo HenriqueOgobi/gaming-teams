7d6bb85d4080fccbad8796745c9618a0f8a7cfaf
Nikaum-js <nikkolas21082003@gmail.com>
2024-01-15 16:29:56 -0300

    feat: corrigindo erro no styled-components

--- App.tsx
+++ App.tsx
@@ -1,9 +1,8 @@
 import { Groups } from '@screens/Groups';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 
 export default function App() {
   return (
-      <Groups/>
+    <Groups/>
   );
 }
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import { Container, Title } from './styles';
 
 export function Groups() {
