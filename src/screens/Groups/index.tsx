ab5841e701db65949322d2dd96941de715221dcb
Henrique Oliveira Gobi <102251338+HenriqueOgobi@users.noreply.github.com>
2024-01-15 10:09:14 -0300

    im inicial styling this app

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,15 +2,15 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export  function Groups() {
   return (
-      <Text>Groups! Hello React-Native</Text>
+      <Text style={styles.container}> Groups! Hello React-Native</Text>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+   marginTop: 100,
+   justifyContent:'center',
+    color: 'black',
+   
   },
 });
